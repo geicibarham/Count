d24907f93d0ec6af6348e4a9eee17b83382183ce
geici <geicianecosta43@gmail.com>
2022-11-21 22:53:03 -0500

    first commit

---
+++ src/components/landing/landing.js
@@ -0,0 +1,15 @@
+import './_landing.scss'
+
+const Landing = ()=>{
+    return (
+        <section className='landing'>
+            <div className='btn-container'>
+      <button className='firstBtn'>Level 1</button> 
+      <button className='secondBtn'>Level 2</button>
+      <button className='thirdBtn'> Level 3</button>
+      </div>
+      </section>
+    )
+}
+
+export default Landing;
