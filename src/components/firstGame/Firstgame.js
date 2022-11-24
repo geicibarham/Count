2156e6302138e5b9da0e39bd238c104664836ba0
geici <geicianecosta43@gmail.com>
2022-11-23 22:49:29 -0500

    adding logic for saving to local storage

--- src/components/firstGame/Firstgame.js
+++ src/components/firstGame/Firstgame.js
@@ -2,23 +2,34 @@ import "./_first.scss";
 import React, { useState } from "react";
 import questions from "../../assets/data/data";
 
-
 const Firstgame = () => {
   const [showResults, setShowResults] = useState(false);
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [score, setScore] = useState(0);
+  const [gameisover, setGameisOver] = useState(false);
 
   const optionClicked = (isCorrect) => {
     // Increment the score
     if (isCorrect) {
-      setScore(score + 1);
+      setScore(score + 10);
     }
 
     if (currentQuestion + 1 < questions.length) {
       setCurrentQuestion(currentQuestion + 1);
     } else {
       setShowResults(true);
+      setGameisOver(true);
+      // console.log(score)
+      // if (score > 0) {
+      //   scorearr.push(score)
+      // }
+      // console.log(scorearr)
+      // localStorage.setItem('scores', JSON.stringify(scorearr))
     }
+    // if (localStorage.getItem('score')) {
+
+    //   scorearr = JSON.parse(localStorage.getItem('score'))
+    // }
   };
 
   const restartGame = () => {
@@ -27,15 +38,28 @@ const Firstgame = () => {
     setShowResults(false);
   };
 
+  let arr = [];
+  if (localStorage.getItem("scores")) {
+    arr = JSON.parse(localStorage.getItem("scores"));
+  }
+  if (score > 0 && gameisover === true) {
+    arr.push(score);
+  }
+
+  localStorage.setItem("scores", JSON.stringify(arr));
+
+  console.log(arr);
+
   return (
     <div className="firstgame">
-    
-      <h2 className='comic'>Score {score}</h2>
+      <h2 className="comic">Score {score}</h2>
 
       {showResults ? (
         <div className="final-result">
           <h3 className="comic">Final Score is {score}</h3>
-          <button className="comic" onClick={restartGame}>RESTART</button>
+          <button className="comic" onClick={restartGame}>
+            RESTART
+          </button>
         </div>
       ) : (
         <div className="question-card comic">
@@ -44,23 +68,24 @@ const Firstgame = () => {
           </h4>
 
           <p className="question-text">{questions[currentQuestion].text}</p>
-          
 
-          <img className="candy-icon"
-          src={questions[currentQuestion].image} alt="donuts" />
+          <img
+            className="candy-icon"
+            src={questions[currentQuestion].image}
+            alt="donuts"
+          />
 
           {questions[currentQuestion].options.map((option, index) => {
             return (
-              <div key={option.id} className="btn-container comic">
-                
-                
-                <button className={"btn"+index + 1 }
+              <span key={option.id} className="btn-container comic">
+                <button
+                  className={"btn" + index + 1}
                   key={option.id}
                   onClick={() => optionClicked(option.isCorrect)}
                 >
                   {option.text}
                 </button>
-              </div>
+              </span>
             );
           })}
         </div>
