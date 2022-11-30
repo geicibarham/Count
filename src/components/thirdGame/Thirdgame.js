fa171c641454b6129bbe5e2fb96ed22c5e014baf
geici <geicianecosta43@gmail.com>
2022-11-29 22:27:57 -0500

    working on game logic, will have to create a state for random number as well

--- src/components/thirdGame/Thirdgame.js
+++ src/components/thirdGame/Thirdgame.js
@@ -1,29 +1,44 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 const numbers = [9, 16, 25, 81, 49, 36, 64, 100];
 const randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
 const Third = () => {
   const [data, setData] = useState(0);
+  const answer = useRef();
+  const [score, setScore] = useState(0);
 
   useEffect(() => {
     axios
       .get(`http://api.mathjs.org/v4/?expr=sqrt(${randomNumber})`)
       .then((res) => {
-        setData(res.data);
+        setData(res.data.toString());
       });
   }, []);
 
-  return(
+  const HandleSubmit = (e) => {
+    e.preventDefault();
+    const enteredAnswer = answer.current?.value;
+    if (data === enteredAnswer) {
+      console.log("correct");
+      setScore(score + 20);
+    } else {
+      console.log("wrong");
+    }
+  };
+
+  console.log(typeof data);
+  return (
     <section className="outer_container">
-
-        <form className="form">
-            <h2>√{randomNumber}</h2>
-            <input type="number" />
-            <button className="comic btn-general">SEND</button>
-
-        </form>
-
-    </section>)
+        <p>{score}</p>
+      <form className="form">
+        <h2>√{randomNumber}</h2>
+        <input ref={answer} type="number" />
+        <button onClick={HandleSubmit} className="comic btn-general">
+          SEND
+        </button>
+      </form>
+    </section>
+  );
 };
 
 export default Third;
